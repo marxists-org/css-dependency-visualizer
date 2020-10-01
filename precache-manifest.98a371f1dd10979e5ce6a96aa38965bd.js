5f6c96e7f86518adb564073dd9fabe8e38568a7c
Philip Mooney <philipm@marxists.org>
2020-09-30 18:01:38 -0700

    Updates

---
+++ precache-manifest.98a371f1dd10979e5ce6a96aa38965bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d47c9a8692d1e02a440133cb3221e4b",
+    "url": "/css-dependency-visualizer/index.html"
+  },
+  {
+    "revision": "275b7675e3938410a73e",
+    "url": "/css-dependency-visualizer/static/css/2.6db69bf1.chunk.css"
+  },
+  {
+    "revision": "a0a9b8144bbccba369ad",
+    "url": "/css-dependency-visualizer/static/css/main.f29c7569.chunk.css"
+  },
+  {
+    "revision": "275b7675e3938410a73e",
+    "url": "/css-dependency-visualizer/static/js/2.ebbbef9c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/css-dependency-visualizer/static/js/2.ebbbef9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0a9b8144bbccba369ad",
+    "url": "/css-dependency-visualizer/static/js/main.34df08ea.chunk.js"
+  },
+  {
+    "revision": "dce87d2071cf39a061df",
+    "url": "/css-dependency-visualizer/static/js/runtime-main.fdf04903.js"
+  }
+]);
