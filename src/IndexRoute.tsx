673b66d2dc0b1b32123ef02728017398dad13945
Philip Mooney <philipm@marxists.org>
2020-11-12 09:55:24 -0800

    Add loading indicator for initial load.

--- src/IndexRoute.tsx
+++ src/IndexRoute.tsx
@@ -1,6 +1,7 @@
 import AppContext from './AppContext';
 import Collection from './Collection';
 import Donut from './Donut';
+import Loading from './Loading';
 import React, { useContext} from 'react';
 import Stats from './Stats';
 import {RouteComponentProps, useLocation} from 'react-router';
@@ -12,16 +13,45 @@ type IndexParams = {
 
 type Props = RouteComponentProps<IndexParams>;
 
+function LoadingPage(props: {}) {
+  return (
+    <>
+      <div className="Curtain">
+        <Loading/>
+      </div>
+      <div className="AppTopViz">
+        <div style={{width: "400px", height: "400px"}}>
+        </div>
+      </div>
+      <div className="AppTopStats">
+      </div>
+      <div className="Header">
+        <div className="Header_name">
+          Name
+        </div>
+        <div className="Header_bar"></div>
+        <div className="Header_count">
+          Count
+        </div>
+        <div className="Header_percentage">
+          Percent
+        </div>
+      </div>
+      <div className="AppList">
+      </div>
+    </>
+  );
+}
+
 function IndexRoute(props: Props) {
   const { data } = useContext(AppContext);
   const location = useLocation();
-  if (data == null) return null;
+  if (data == null) return <LoadingPage />;
   const pathChunks = location.pathname.substr(1).split('/');
   const selectedNode = pathChunks[pathChunks.length - 1];
   const node = data.get(selectedNode === "" ? "ALL_CSS" : selectedNode);
-
   return node == null
-    ? <>loading...</>
+    ? null
     : (<>
         <div className="AppTopViz">
           <div style={{width: "400px", height: "400px"}}>
---
+++ src/Loading.tsx
@@ -0,0 +1,10 @@
+import './Loading.css';
+import * as React from 'react';
+
+function Loading() {
+  return (
+    <div className="loading"></div>
+  );
+}
+
+export default Loading;
